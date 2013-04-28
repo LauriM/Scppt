00ce3739424df875fe35eb895f4057354ce24dd2
Lauri Makinen <lauri.sakari.makinen@gmail.com>
2013-04-28 14:20:28 +0300

    Prefixed OK/FAIL collisions in ncurses lib

--- scppt.h
+++ scppt.h
@@ -6,8 +6,8 @@
 
 #define LINEINFO printf("["TO_STRING(__FILE__)":"TO_STRING(__LINE__)"] ");
 
-#define OK REPORT_TIMER PRINTLN("OK"); testOks++;
-#define FAIL REPORT_TIMER PRINTLN("FAIL"); testFails++;
+#define SCPPT_OK REPORT_TIMER PRINTLN("OK"); testOks++;
+#define SCPPT_FAIL REPORT_TIMER PRINTLN("FAIL"); testFails++;
 
 #define ECHO(p_msg) LINEINFO printf(p_msg"\n");
 #define PRINT(p_msg) printf(p_msg);
@@ -19,13 +19,13 @@
 
 #define SCPPT_COMPARE(p_name,p_var1,p_check,p_var2) LINEINFO START_TIMER PRINT("< "p_name" > ") \
 	if(p_var1 p_check p_var2){ \
-		OK\
+		SCPPT_OK\
 	}else{\
-		FAIL\
+		SCPPT_FAIL\
 	}
 
 #define SCPPT_START clock_t scppt_timer; int testFails = 0; int testOks = 0; clock_t scppt_starttime = clock();
 #define SCPPT_END printf("Ok: %i Fail: %i\n",testOks,testFails); \
 	clock_t scppt_exectime = clock() - scppt_starttime;\
-	double scppt_time = ((double)scppt_exectime)/CLOCKS_PER_SEC;\
-	printf("Exec time: %f\n",scppt_time);
+double scppt_time = ((double)scppt_exectime)/CLOCKS_PER_SEC;\
+printf("Exec time: %f\n",scppt_time);
